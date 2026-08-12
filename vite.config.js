ba01e1b5ef85ccd5c59a81df7fcbc8d3028d01fb
Harshal Patil <harshal1292000@gmail.com>
2026-08-12 23:10:23 +0530

    deploy base url

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Shri_Ganesh_Paint/',
   server: {
     port: 5173,
     host: true
+     
   }
 })
